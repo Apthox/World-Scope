b819f037f0dec6efdeeb40ce8ff47cbff6cb2235
Sam poplack <sampoplack@Sams-Macbook-Pro-3.local>
2020-04-13 19:53:03 -0700

    adding db connection

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ var logger = require('morgan');
 var passport = require("passport");
 var bodyParser = require('body-parser');
 var localStrategy = require('passport-local');
-
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountRouter = require('./routes/account');
 var gameRouter = require('./routes/game');
 var leaderboardRouter = require('./routes/leaderboard');
+
+var uri = "mongodb://sampop:Project2@cluster0-shard-00-00-hnxfk.mongodb.net:27017,cluster0-shard-00-01-hnxfk.mongodb.net:27017,cluster0-shard-00-02-hnxfk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+let dbconnection;
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true },(err,db) => {
+    dbconnection = db.db('project3');
+});
 
 
 
