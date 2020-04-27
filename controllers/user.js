1adaaa8503084b947cfb5de7f22557671af70ade
Yessica Torrez Hernandez <yessi@Yessicas-MacBook-Pro.local>
2020-04-27 14:23:28 -0700

    changes

---
+++ controllers/user.js
@@ -0,0 +1,11 @@
+/*
+
+Login User Controller
+
+Purpose : Use Model user.js to check it exists and grab UID, then return success and if successful pass back UID
+
+*/
+
+module.exports.loginUser = function () {
+
+}
