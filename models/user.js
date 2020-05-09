ccddbe823b6d2c161c1a605d602142b40c7c6caa
Apthox <kevin@apthos.net>
2020-05-08 22:30:20 -0700

    pages

--- models/user.js
+++ models/user.js
@@ -1,9 +1,8 @@
-
 var dbconnection = require('./dbconnection');
 var bcrypt = require('bcrypt');
 
 // return object with user id
-module.exports.authenticate_user = async function (username, password) {
+module.exports.authenticate_user = async function(username, password) {
 
     let resp = {
         "success": true,
@@ -14,30 +13,32 @@ module.exports.authenticate_user = async function (username, password) {
     // check database if someone with username exists
     let dbo = await dbconnection.get_dbo_instance();
     // if exits get passwor
-    let users = await dbo.collection('users').find({username:username}).toArray();
-    if(users.length < 1) {
+    let users = await dbo.collection('users').find({ username: username }).toArray();
+    if (users.length < 1) {
         resp["success"] = false;
         return resp;
     }
 
     let passwordHash = users[0].password;
     // user bcrypt to check if password matches hash
-    let success = await new Promise((resolve,reject) => {
-        bcrypt.compare(password, passwordHash, function(err,result) {
-            if(err) {
+    let success = await new Promise((resolve, reject) => {
+        bcrypt.compare(password, passwordHash, function(err, result) {
+            if (err) {
+                resp["msg"] = "System Error!";
                 reject(false);
             }
-            if(result) {
+            if (result) {
                 resolve(true);
             } else {
                 resolve(false);
+                resp["msg"] = "incorrect password/username";
             }
         });
     });
-    
+
     // return true if matches
     resp["success"] = success;
-    resp["UUID"] = users[0]._id;
+    resp["user"] = users[0];
     return success;
 };
 
@@ -49,18 +50,19 @@ module.exports.createUser = async function(username, password, salt) {
     let hash = await new Promise((resolve, reject) => {
         bcrypt.genSalt(12).then(salt => {
             bcrypt.hash(password, salt, function(err, hash) {
-            if (err) {
-                reject(err);
-            }
-            resolve(hash);
-        })});
+                if (err) {
+                    reject(err);
+                }
+                resolve(hash);
+            })
+        });
     });
 
     console.log("Stored Hash > " + hash);
 
     dbo.collection('users').insertOne({
-        username:username,
-        password:hash
+        username: username,
+        password: hash
     });
 
     return true;
@@ -68,22 +70,22 @@ module.exports.createUser = async function(username, password, salt) {
 }
 
 module.exports.get_users = async function() {
-    let dbo = await dbconnection.get_dbo_instance();
-    dbo.collection('users').find();
-}
-/*
-Please use Zoom to record a video of your project 03.
-Each team member must talk about what they have contributed and show off their code.
-Do this by sharing your screen and walking through the work you have done.
-Answer the following:
-What have you done?
-What will you do?
-What roadblocks are there?
-This assignment serves the following purpose
-Ensures everyone is working
-Gets us used to using Zoom to record our project
-Secretly there will be a very similar assignment for your FINAL Submission
-Lets me see how the application is SUPPOSED to work
+        let dbo = await dbconnection.get_dbo_instance();
+        dbo.collection('users').find();
+    }
+    /*
+    Please use Zoom to record a video of your project 03.
+    Each team member must talk about what they have contributed and show off their code.
+    Do this by sharing your screen and walking through the work you have done.
+    Answer the following:
+    What have you done?
+    What will you do?
+    What roadblocks are there?
+    This assignment serves the following purpose
+    Ensures everyone is working
+    Gets us used to using Zoom to record our project
+    Secretly there will be a very similar assignment for your FINAL Submission
+    Lets me see how the application is SUPPOSED to work
 
 
-*/
+    */
--- routes/account.js
+++ routes/account.js
@@ -11,13 +11,15 @@ router.get('/login', function(req, res, next) {
     res.render('login', { title: 'Log in Page' });
 });
 
-router.post('/login',function(req,res,next) {
-    var userdata = User.authenticate_user(req.body.username,req.body.password);
-    if(userdata) {
+router.post('/login', async function(req, res, next) {
+    var data = await User.authenticate_user(req.body.username, req.body.password);
+    console.log(data);
+    if (data["success"]) {
+        console.log("Was authenticated!");
         res.redirect('/')
-    }
-    else {
-        res.redirect('/login',{message: "error logging in"});
+    } else {
+        console.log("Was not authenticated!");
+        res.render('/login', { message: "error logging in" });
     }
 });
 
@@ -26,18 +28,17 @@ router.get('/logout', function(req, res, next) {
     res.render('logout', { title: 'Express' });
 });
 
-router.post('/signup',function(req, res, next) {
+router.post('/signup', function(req, res, next) {
     console.log(User.prototype);
     console.log("Username > " + req.body.username);
     console.log("Password > " + req.body.password);
     var user = User.createUser(req.body.username, req.body.password);
     console.log(user.prototype);
-    if(user) {
+    if (user) {
         console.log(user);
         res.redirect('/');
-    }
-    else {
-        res.redirect('/signup',{message: "error creating user: User Already Exists"});
+    } else {
+        res.redirect('/signup', { message: "error creating user: User Already Exists" });
     }
 })
 
